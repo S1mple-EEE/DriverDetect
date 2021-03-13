d29c1b5ea3989d8aa1ee412cdbfdd355096bf0a5
2283861113@qq.com <2283861113@qq.com>
2021-03-13 16:18:16 +0800

    2021.3.13 16:16 master master推进到lwz,lby,dd合并版本 运行无错误，后续pull时只要在setting.py中PostgreSQL密码即可

--- weixindjango/app/gaze_tracking/gaze_tracking.py
+++ weixindjango/app/gaze_tracking/gaze_tracking.py
@@ -24,7 +24,7 @@ class GazeTracking(object):
 
         # _predictor is used to get facial landmarks of a given face
         cwd = os.path.abspath(os.path.dirname(__file__))
-        model_path = os.path.abspath(os.path.join(cwd, "trained_models/shape_predictor_68_face_landmarks.dat"))
+        model_path = os.path.abspath(os.path.join(cwd, "models/shape_predictor_68_face_landmarks.dat"))
         self._predictor = dlib.shape_predictor(model_path)
 
     @property
